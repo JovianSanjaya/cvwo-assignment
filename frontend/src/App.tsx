eb279a7875ea75167885f16739c9efbefc1458dd
Jovian <e1668492@u.nus.edu>
2026-01-25 23:22:07 +0800

    oauth resolve

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,21 +10,27 @@ import Landing from "./pages/Landing";
 
 
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
+
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
 
-          <Route path="/topics/:id/posts" element={<TopicPosts />} />
-          <Route path="/topics/:topicId/posts/:postId/comments" element={<PostComments />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+            <Route path="/topics/:id/posts" element={<TopicPosts />} />
+            <Route path="/topics/:topicId/posts/:postId/comments" element={<PostComments />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   )
 
 }
